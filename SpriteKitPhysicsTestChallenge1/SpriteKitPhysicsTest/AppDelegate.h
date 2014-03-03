dca37d3cd22a4f7b6a48dbc0a578e4a5bbf2caa3
Samir <samirnaimi@gmail.com>
2014-03-03 00:59:03 -0500

    Trying to upload hw

---
+++ SpriteKitPhysicsTestChallenge1/SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Samir Naimi on 3/2/14.
+//  Copyright (c) 2014 Samir Naimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
